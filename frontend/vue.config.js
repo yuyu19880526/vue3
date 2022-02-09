7557ec8f905f3a1c66053ba694cd7ded9927d4a6
liujingmin <553036392@qq.com>
2022-02-09 17:42:03 +0800

    fix

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-    css: {
-      loaderOptions: {
-        less: {
-          lessOptions: {
-            modifyVars: {
-              'primary-color': '#1DA57A',
-              'link-color': '#1DA57A',
-              'border-radius-base': '2px',
-            },
-            javascriptEnabled: true,
+  outputDir: './web',
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          modifyVars: {
+            'primary-color': '#1DA57A',
+            'link-color': '#1DA57A',
+            'border-radius-base': '2px',
           },
+          javascriptEnabled: true,
         },
       },
     },
-  }
+  },
+}
